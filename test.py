d7c545ac8ee877f7714283ec004d05f1f10da042
KeysCreator <143636898+KeysCreator@users.noreply.github.com>
2024-07-24 18:06:55 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,10 @@
-# server.py
 from flask import Flask
-from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app, async_mode='gevent')
 
-@socketio.on('message')
-def handle_message(msg):
-    print(f'Received message: {msg}')
-    send('Hi!')
+@app.route('/')
+def hello():
+    return "Hello!"
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    app.run(debug=True)
